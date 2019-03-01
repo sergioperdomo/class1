6125fcf26a79876e5efc5ec68d780093c1f29ab5
USUARIO <USUARIO@PC16>
2019-02-28 21:45:30 -0500

    add variables

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,11 +4,11 @@ package prueba;
 
 public class Prueba {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero1=0;
+        int numero2=0;
+        
     }
     
 }
